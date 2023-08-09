e734707dd6ba11eb9a463b876e7239ea73a82bbf
zasamy <zainebzahiri@gmail.com>
2023-08-09 13:23:30 -0700

    5-print_comb2.py file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for num in range(100):
     if num == 99:
         print("{}". format(num))
     else :
-        print ("{}{}". format(num // 10, num % 10), end=", ")
+        print ("{:d}{:d}". format(num // 10, num % 10), end=", ")

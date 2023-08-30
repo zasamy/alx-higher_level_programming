2e82e71c9d476ccca128c48f4063bd221e0038a5
zasamy <zainebzahiri@gmail.com>
2023-08-29 17:40:55 -0700

    6-square.py file

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """creates a  sqare."""
+
+    def __init__(self, size):
+        """present instance attribute."""
+        self.size = size
+
+    @property
+    def size(self):
+        """retirieve size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """gives area"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """square with the character #"""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")

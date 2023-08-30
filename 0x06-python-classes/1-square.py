6c1db24ade009b6bfbcfb77e9277774939758dd1
zasamy <zainebzahiri@gmail.com>
2023-08-29 17:28:46 -0700

    1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""defines a class square"""
+
+
+class Square:
+    """creates a squres"""
+
+    def __init__(self, size):
+        """present the instance attribute."""
+
+        self.__size = size

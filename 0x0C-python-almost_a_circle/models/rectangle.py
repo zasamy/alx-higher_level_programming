5bb3815042bdc646ca63314a99e7018457e69d8b
zasamy <zainebzahiri@gmail.com>
2023-09-21 17:00:40 -0700

    rectangle.py file

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""rectangle module"""
+
+from models.base import Base
+import sys
+
+
+class Rectangle(Base):
+    """rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize attributes"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """retrieve width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """retrieve x"""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """set x"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """retrieve y"""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """set y"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value

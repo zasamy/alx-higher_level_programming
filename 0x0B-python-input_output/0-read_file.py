34f578b49ebe37be780c4cf9c4b73ca45bf2fa58
zasamy <zainebzahiri@gmail.com>
2024-04-16 11:09:36 -0700

    0-read_file.py file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Module containing the function read_file"""
+""" Module that contains a function that reads from a file """
 
 
 def read_file(filename=""):
-    """Reads a file and prints to stdout.
+    """ Function that reads from a file
 
     Args:
-        filename (str, optional): name of file to read. Defaults to "".
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
     """
+
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')

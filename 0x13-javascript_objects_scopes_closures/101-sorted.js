3abd8f4ffdea6f5e28a14a4c2d083662b3b2ba09
zasamy <zainebzahiri@gmail.com>
2024-06-19 13:32:24 -0700

    101-sorted.js file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const dict = require('./101-data').dict;
+const initDict = require('./101-data').dict;
 const newDict = {};
 
-Object.keys(dict).map(function (key) {
-  if (!Array.isArray(newDict[dict[key]])) {
-    newDict[dict[key]] = [];
+for (const key in initDict) {
+  if (newDict[initDict[key]] === undefined) {
+    newDict[initDict[key]] = [];
   }
-  newDict[dict[key]].push(key);
-});
-
+  newDict[initDict[key]].push(key);
+}
 console.log(newDict);
